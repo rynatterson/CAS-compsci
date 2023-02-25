1b3bbff42d3390836cf5a821d60ab8384c8cb812
rynatterson <126192950+rynatterson@users.noreply.github.com>
2023-02-25 11:05:24 -0800

    Nasa epic photo- Rynatterson

---
+++ Nasa epic selfie/Nasa epic selfie/Nasa_epic_selfieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nasa_epic_selfieApp.swift
+//  Nasa epic selfie
+//
+//  Created by Ry Natterson on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Nasa_epic_selfieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
